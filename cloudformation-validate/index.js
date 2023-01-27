807d548bfc3d9e79f35e87f14a1703270144cf0b
Luke Sheard <luke.sheard@letsdothis.com>
2023-01-27 15:37:25 +0000

    Validate cloudformation files on PR

--- cloudformation-validate/index.js
+++ cloudformation-validate/index.js
@@ -14,51 +14,56 @@ const cloudformationFile = path.join(
   "cloudformation",
   "cloudformation.yaml"
 );
-if (fs.existsSync(cloudformationFile)) {
-  const environmentFile = path.join(
-    process.cwd(),
-    "configs",
-    environment + ".json"
-  );
-  const environmentText = fs.readFileSync(environmentFile, {
-    encoding: "utf-8",
+
+const environmentFile = path.join(
+  process.cwd(),
+  "configs",
+  environment + ".json"
+);
+
+if (!fs.existsSync(cloudformationFile)) {
+  return console.log("No Cloudformation file");
+}
+
+if (!fs.existsSync(environmentFile)) {
+  return console.log("No Environment file ");
+}
+
+const environmentText = fs.readFileSync(environmentFile, {
+  encoding: "utf-8",
+});
+const environmentConfig = JSON.parse(environmentText);
+
+const cloudformationText = fs.readFileSync(cloudformationFile, {
+  encoding: "utf-8",
+});
+const cloudformationConfig = yamlParse(cloudformationText);
+
+const allowedParameters = new Set(
+  Object.keys(cloudformationConfig.Parameters || {})
+);
+const parsedParameters = Object.keys(environmentConfig.Parameters || {});
+
+const disallowedParameters = parsedParameters
+  .filter((key) => !allowedParameters.has(key))
+  .sort();
+
+if (disallowedParameters.length) {
+  failureMessages = [];
+  failureMessages.push(`Found unexpected Parameters`);
+  disallowedParameters.forEach((parameter) => {
+    failureMessages.push(`     ${parameter}`);
   });
-  const environmentConfig = JSON.parse(environmentText);
+  const failureMessage = failureMessages.join("\n");
+  console.error(failureMessage);
+  core.setFailed(failureMessage);
+}
 
-  const cloudformationText = fs.readFileSync(cloudformationFile, {
-    encoding: "utf-8",
+if (process.argv.indexOf("--fix")) {
+  console.info("Fixing...");
+  disallowedParameters.forEach((parameter) => {
+    environmentConfig.Parameters[parameter] = undefined;
   });
-  const cloudformationConfig = yamlParse(cloudformationText);
-
-  const allowedParameters = new Set(
-    Object.keys(cloudformationConfig.Parameters || {})
-  );
-  const parsedParameters = Object.keys(environmentConfig.Parameters || {});
-
-  const disallowedParameters = parsedParameters
-    .filter((key) => !allowedParameters.has(key))
-    .sort();
-
-  if (disallowedParameters.length) {
-    failureMessages = [];
-    failureMessages.push(`Found unexpected Parameters`);
-    disallowedParameters.forEach((parameter) => {
-      failureMessages.push(`     ${parameter}`);
-    });
-    const failureMessage = failureMessages.join("\n");
-    console.error(failureMessage);
-    core.setFailed(failureMessage);
-  }
-
-  if (process.argv.indexOf("--fix")) {
-    console.info("Fixing...");
-    disallowedParameters.forEach((parameter) => {
-      environmentConfig.Parameters[parameter] = undefined;
-    });
-    environmentConfig.Parameters = sortKeys(environmentConfig.Parameters);
-    fs.writeFileSync(
-      environmentFile,
-      JSON.stringify(environmentConfig, null, 2)
-    );
-  }
+  environmentConfig.Parameters = sortKeys(environmentConfig.Parameters);
+  fs.writeFileSync(environmentFile, JSON.stringify(environmentConfig, null, 2));
 }
